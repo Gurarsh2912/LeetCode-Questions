133644cc7b1db210759297c93e62612983ec5458
Gurarsh <gurarshdeep7@gmail.com>
2026-09-24 00:56:16 +0530

    Time: 4 ms (41.06%), Space: 11.5 MB (55.85%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     string predictPartyVictory(string senate) {
+        int n = senate.size();
         queue<int> qR;
         queue<int> qD;
-        int i = 0;
+        int i=0;
         for(char ch : senate){
             if(ch=='R'){
                 qR.push(i);
@@ -14,18 +15,19 @@ public:
                 i++;
             }
         }
+
         while(!qR.empty() && !qD.empty()){
-            if(qR.front()<qD.front()){
+            if(qR.front() < qD.front()){
                 int idx = qR.front();
                 qR.pop();
                 qD.pop();
-                qR.push(idx+i);
+                qR.push(idx+n);
             }
             else{
                 int idx = qD.front();
                 qR.pop();
                 qD.pop();
-                qD.push(idx+i);
+                qD.push(idx+n);
             }
         }
         return !qR.empty() ? "Radiant" : "Dire";
